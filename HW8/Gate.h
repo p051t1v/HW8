6716faee305b01ceaf4ea0cb1c12beb5cc6e6b99
p051t1v <tealjj22@gmail.com>
2024-11-13 15:47:40 -0500

    Getting Gate.h right

---
+++ HW8/Gate.h
@@ -0,0 +1,16 @@
+#ifndef GATE_H
+#define GATE_H
+
+#include <iostream>
+#include <string>
+
+class Gate {
+	private:
+		string type;
+		int delay;
+		Wire input1;
+		Wire input2;
+		Wire output;
+	public:
+		Gate(string type, Gate )
+};
